f14d34751639aac23fa870f81370d0f03b270020
Pedro Leal <lealpedro2009@gmail.com>
2025-07-29 19:12:34 +0100

    refactor: code style

--- src/tests/models/orderSpec.ts
+++ src/tests/models/orderSpec.ts
@@ -10,8 +10,7 @@ describe('Order Model', () => {
     await conn.query('DELETE FROM orders');
     await conn.query('DELETE FROM users');
     const userResult = await conn.query(
-      'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3,$4) RETURNING id',
-      ['John', 'Doe', 'john@mail.com','password123']
+      'INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3,$4) RETURNING id', ['John', 'Doe', 'john@mail.com','password123']
     );
     userId = userResult.rows[0].id;
     await conn.query('INSERT INTO orders (user_id, status) VALUES ($1, $2)', [userId, 'active']);
